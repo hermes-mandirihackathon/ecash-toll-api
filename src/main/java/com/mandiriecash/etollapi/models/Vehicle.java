db6ad9bbc8ab0165f56895860a72d42552474c1d
Muhammad Yafi <yafithekid212@gmail.com>
2016-01-27 10:55:30 +0700

    add sample for creating rest api

---
+++ src/main/java/com/mandiriecash/etollapi/models/Vehicle.java
@@ -0,0 +1,43 @@
+package com.mandiriecash.etollapi.models;
+
+public class Vehicle
+{
+    private int id;
+    private String name;
+    private String plateNo;
+    private String photoUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlateNo() {
+        return plateNo;
+    }
+
+    public void setPlateNo(String plateNo) {
+        this.plateNo = plateNo;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
+
+
